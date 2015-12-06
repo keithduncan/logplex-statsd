fd09608ebea3d0b0da385f6f926e56cf255d8018
Keith Duncan <keith.duncan@github.com>
2015-12-06 13:05:21 +1100

    Start the app skeleton

---
+++ src/Main.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import System.Environment
+import Web.Scotty
+import Network.Wai.Middleware.RequestLogger
+
+import Data.Text as T
+
+import Text.Logplex.Parser
+import Network.Statsd
+
+main :: IO ()
+main = do
+  env <- getEnvironment
+  let port = maybe 3000 read $ lookup "PORT" env :: Int
+  server port
+
+server :: Int -> IO ()
+server port = scotty port $ do
+  middleware logStdoutDev
+
+  get "/" $ do
+    greeting <- param "greeting"
+    html $ mconcat ["Hello, ", greeting, "!"]
+
+{-
+server :: Int -> IO ()
+server port = scotty port $ do
+  post "/:app_name/logs" $ do
+    app <- param "app_name" :: T.Text
+
+    sendMetrics <- getWhitelistedApp app
+
+    if sendMetrics
+    then error "cannot send metrics"
+    else error "cannot render 404"
+-}
+
+{- This may be updated to check a static list, consult a file, environment
+   variable, or another web service -}
+getWhitelistedApp :: String -> IO Bool
+getWhitelistedApp = const $ return True
