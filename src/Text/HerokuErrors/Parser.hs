926ac1f552b13840ec897e1be54dbdda9ad2febf
Keith Duncan <keith.duncan@github.com>
2015-12-07 20:12:50 +1100

    Start R* error parsing

--- src/Text/HerokuErrors/Parser.hs
+++ src/Text/HerokuErrors/Parser.hs
@@ -35,7 +35,11 @@ parseHerokuError content = do
   TODO this just parses H class errors, handle R and L class errors too
   <https://devcenter.heroku.com/articles/error-codes>
 -}
-herokuError = P.sepBy kvPair P.space <* P.eof
+herokuError = P.choice $ P.try <$> [ hError, rError ]
+
+-- hError
+
+hError = P.sepBy kvPair P.space <* P.eof
 
 type Key = String
 type Value = String
@@ -55,3 +59,12 @@ escaped echar chars = let echars = echar:chars
                        in P.noneOf echars P.<|> P.choice (fmap (P.try . (P.char echar >>) . P.char) echars)
 
 plainValue = P.manyTill P.alphaNum P.space
+
+-- rError
+
+rError = do
+  code <- P.string "Error" >> P.space >> rVal
+  desc <- P.space >> P.between (P.char '(') (P.char ')') (P.many1 $ P.noneOf ")")
+  return [("at", "error"), ("code", code), ("desc", desc)]
+
+rVal = liftM2 (:) (P.char 'R') (P.many1 P.digit)
