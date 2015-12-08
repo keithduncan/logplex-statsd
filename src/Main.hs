2357274aadda99a9b99d4bea5be93d99285ac75e
Keith Duncan <keith.duncan@github.com>
2015-12-08 16:14:52 +1100

    Specify that each app can have its own auth

--- src/Main.hs
+++ src/Main.hs
@@ -28,13 +28,16 @@ server port = scotty port $ do
   middleware logStdoutDev
 
   post "/:app_name/logs" $ do
+    auth <- header "Authorization"
+
 {-
-    1. check authentication
-    2. check content-type
-    3. parse body as application/logplex-1
-    4. map log entry messages
-    5. parse messages as heroku errors
-    6. map each error to a statsd client increment action
+    1. get app name
+    2. check per-app authentication
+    3. check content-type
+    4. parse body as application/logplex-1
+    5. map log entry messages
+    6. parse messages as heroku errors
+    7. map each error to a statsd client increment action
 
     app <- param "app_name"
     contentType <- header "Content-Type"
