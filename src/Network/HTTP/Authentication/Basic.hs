eac36b91e568ebccd26ecc5e7298d1fd21567c5e
Keith Duncan <keith.duncan@github.com>
2015-12-09 20:00:32 +1100

    Use a better monad instance of Either

--- src/Network/HTTP/Authentication/Basic.hs
+++ src/Network/HTTP/Authentication/Basic.hs
@@ -4,33 +4,36 @@ module Network.HTTP.Authentication.Basic (
 ) where
 
 import Control.Monad
+import Control.Monad.Except
 
-import Data.ByteString.Base64
+import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8 as B
 
 import Text.ParserCombinators.Parsec
 
 data Credentials = Credentials { getUsername :: String
                                , getPassword :: String
-                               }
+                               } deriving (Show)
 
-instance Show Credentials where
-  show (Credentials user pass) = let concat' = mconcat [user, ":", pass]
-                                     encoded = (encode . B.pack) concat'
-                                  in "Basic " ++ B.unpack encoded
+encode :: Credentials -> String
+encode c = let concat' = mconcat [getUsername c, ":", getPassword c]
+               encoded = (B.unpack . B64.encode . B.pack) concat'
+            in "Basic " ++ encoded
 
 parseCredentials :: String -> Either ParseError Credentials
-parseCredentials content = do
-  decoded <- parse parseBasicCredentials "(unknown)" content
+parseCredentials = parse parseBasicCredentials "(unknown)"
 
-  let (user, pass) = break (/= ':') decoded
+parseBasicCredentials = do
+  decoded <- string "Basic" >> space >> base64String
 
-  return $ Credentials user pass
+  let pair = break (== ':') decoded
 
-parseBasicCredentials = string "Basic" >> space >> base64String
+  case pair of
+    (user, ':':pass) -> return $ Credentials user pass
+    _                -> fail "not a username password pair"
 
-base64Char = oneOf ("+/" ++ ['0'..'9'] ++ ['A'..'Z'] ++ ['a'..'z'])
+base64Char = oneOf ("+/=" ++ ['0'..'9'] ++ ['A'..'Z'] ++ ['a'..'z'])
 
 base64String = do
   encoded <- many1 base64Char
-  either (fail "not a base64 string") (return . B.unpack) $ decode (B.pack encoded)
+  either fail (return . B.unpack) $ B64.decode (B.pack encoded)
