50aa66c86f0d2ce643b06cf1b09338140b4e3003
Keith Duncan <keith.duncan@github.com>
2015-12-13 18:20:52 +1100

    Use better matcher

--- spec/Text/HerokuErrors/ParserSpec.hs
+++ spec/Text/HerokuErrors/ParserSpec.hs
@@ -4,10 +4,8 @@ import Test.Hspec
 
 import Text.HerokuErrors.Parser
 
-fromRight (Right x) = x
-
 spec = do
   describe "HerokuErrors Parser" $ do
     it "should parse H10 errors" $ do
-      let err = fromRight $ parseHerokuError "at=error code=H10 desc=\"App crashed\" method=GET path=\"/\" host=myapp.herokuapp.com fwd=17.17.17.17 dyno= connect= service= status=503 bytes="
-      err `shouldBe` (HerokuError "H10" "App crashed")
+      let err = parseHerokuError "at=error code=H10 desc=\"App crashed\" method=GET path=\"/\" host=myapp.herokuapp.com fwd=17.17.17.17 dyno= connect= service= status=503 bytes="
+      err `shouldBe` Right (HerokuError "H10" "App crashed")
